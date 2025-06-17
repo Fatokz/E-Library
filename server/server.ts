7337b457d4f64123db5c74f5764be9e102e78f9e
Ambassador <fatokunemmanuel16@gmail.com>
2025-06-17 05:51:37 +0100

    fix: Update CORS origin configuration for production environment

--- server/server.ts
+++ server/server.ts
@@ -22,7 +22,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://booksync-amb.vercel.app/",
+    // origin: "http://localhost:5173",
+    origin: "https://booksync-amb.vercel.app",
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
   })
